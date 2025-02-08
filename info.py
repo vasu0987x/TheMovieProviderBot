75bfcfd2044370f237df5048307faa4fb4361e22
vasu0987x <vasupareek2x4@gmail.com>
2025-02-08 17:28:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -128,7 +128,7 @@ else:
     URL = "http://{}/".format(FQDN)
 
 # add premium logs channel id
-PREMIUM_LOGS = int(environ.get('PREMIUM_LOGS', ''))
+PREMIUM_LOGS = int(environ.get('PREMIUM_LOGS', '-1002149303924'))
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
 LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
